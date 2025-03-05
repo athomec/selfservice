609e447b81a651d94344a7f52f50fa265b2a845b
athomec <athomec@pie.com.tw>
2025-03-05 14:01:43 +0800

    測試調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,5 +31,21 @@ $(function () {//JS開頭
 	$(".js-nav-grid2").click(function () {
 		$(this).parents(".container").find(".col-4").removeClass("col-4").addClass("col-6");
 	})
+	
+	//菜單定位位置調整
+	$('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+        let targetId = $(this).attr('href'); // 取得目標區塊的 ID
+        let targetElement = $(targetId);
+
+        if (targetElement.length) {
+            let offset = targetElement.offset().top; // 取得原始滾動位置
+            let zoomFactor = 2.6; // 設定你的 zoom 值
+            let adjustedOffset = offset / zoomFactor; // 修正 zoom 影響
+
+            $('html, body').animate({
+                scrollTop: adjustedOffset
+            }, 300); // 平滑滾動
+        }
+    });
 
 })//JS尾端	
